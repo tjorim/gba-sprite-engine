596f2019659846e5a66a6b3f35382961aed69190
Jorim Tielemans <tielemans.jorim@gmail.com>
2019-01-27 00:24:34 +0100

    Add level option to StartScene
    Removed unused imports
    Add functions to alter level int

--- bomberboy/src/start_scene.cpp
+++ bomberboy/src/start_scene.cpp
@@ -2,14 +2,14 @@
 // Created by JT
 //
 
-#include <libgba-sprite-engine/sprites/sprite_builder.h>
-#include <libgba-sprite-engine/background/text_stream.h>
-#include <libgba-sprite-engine/gba/tonc_memdef.h>
+//#include <libgba-sprite-engine/background/text_stream.h>
 #include <libgba-sprite-engine/gba_engine.h>
 
-#include "start_scene.h"
 #include "game_scene.h"
 #include "sound.h"
+#include "start_scene.h"
+
+StartScene::StartScene(const std::shared_ptr<GBAEngine> &engine) : Scene(engine) {}
 
 std::vector<Background *> StartScene::backgrounds() {
     return {};
@@ -23,19 +23,39 @@ void StartScene::load() {
     foregroundPalette = std::unique_ptr<ForegroundPaletteManager>(new ForegroundPaletteManager());
     backgroundPalette = std::unique_ptr<BackgroundPaletteManager>(new BackgroundPaletteManager());
 
-    engine->getTimer()->start();
     engine->enqueueMusic(cataclysmic_molten_core, sizeof(cataclysmic_molten_core));
-
-    GameScene* gameScene = new GameScene(engine);
 }
 
 void StartScene::tick(u16 keys) {
-    if(engine->getTimer()->getTotalMsecs() < 5000) {
-        counter++;
-    } else {
-        engine->getTimer()->stop();
+    if (keys & KEY_ACCEPT) {
+        engine->setScene(new GameScene(engine, getLevel()));
+    } else if ((keys & KEY_LEFT) || (keys & KEY_DOWN)) {
+        levelDown();
+    } else if ((keys & KEY_RIGHT) || (keys & KEY_UP)) {
+        levelUp();
     }
+}
 
-    TextStream::instance().setText(std::to_string(counter) + std::string(" frames/5sec"), 5, 1);
-    TextStream::instance().setText(std::string(engine->getTimer()->to_string()), 6, 1);
- }
+int StartScene::getLevel() const {
+    return level;
+}
+
+void StartScene::setLevel(int level) {
+    StartScene::level = level;
+}
+
+void StartScene::levelUp() {
+    if (level < 4) {
+        level++;
+    } else {
+        setLevel(4);
+    }
+}
+
+void StartScene::levelDown() {
+    if (level > 1) {
+        level--;
+    } else {
+        setLevel(1);
+    }
+}
