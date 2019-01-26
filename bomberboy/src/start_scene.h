d30d380e7ace4543b23f56da3cc3b55ef0779d23
Jorim Tielemans <tielemans.jorim@gmail.com>
2019-01-27 00:24:34 +0100

    Add level option to StartScene
    Removed unused imports
    Add functions to alter level int

--- bomberboy/src/start_scene.h
+++ bomberboy/src/start_scene.h
@@ -1,27 +1,34 @@
 //
-// Created by JT
+// Created by Jorim Tielemans
 //
 
 #ifndef GBA_SPRITE_ENGINE_PROJECT_START_SCENE_H
 #define GBA_SPRITE_ENGINE_PROJECT_START_SCENE_H
 
-
 #include <libgba-sprite-engine/scene.h>
 
 class StartScene : public Scene {
 private:
-    int counter = 0;
+    int level = 1;
+
+    void levelUp();
+
+    void levelDown();
 
 public:
+    StartScene(const std::shared_ptr<GBAEngine> &engine);
+
     std::vector<Sprite *> sprites() override;
+
     std::vector<Background *> backgrounds() override;
 
-    StartScene(const std::shared_ptr<GBAEngine> &engine) : Scene(engine) {}
-
     void load() override;
+
     void tick(u16 keys) override;
 
-};
+    int getLevel() const;
 
+    void setLevel(int level);
+};
 
 #endif //GBA_SPRITE_ENGINE_PROJECT_START_SCENE_H
