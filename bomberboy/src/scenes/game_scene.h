14758ac3d2d13d59804bf95b71ba2afb51220f22
Jorim Tielemans <tielemans.jorim@gmail.com>
2019-01-27 15:40:23 +0100

    ja dus echt he

--- bomberboy/src/scenes/game_scene.h
+++ bomberboy/src/scenes/game_scene.h
@@ -5,23 +5,31 @@
 #ifndef GBA_SPRITE_ENGINE_PROJECT_GAME_SCENE_H
 #define GBA_SPRITE_ENGINE_PROJECT_GAME_SCENE_H
 
-
 #include <libgba-sprite-engine/scene.h>
+#include <libgba-sprite-engine/sprites/sprite_builder.h>
+
+#include "../thing/bomb.h"
 
 class GameScene : public Scene {
 private:
     int level = 1;
+    std::vector<std::vector<Thing>> board;
+    std::vector<std::unique_ptr<Bomb>> bombs;
+    std::unique_ptr<SpriteBuilder<Sprite>> spriteBuilder;
+    std::unique_ptr<Sprite> bomSprite;
+
+    std::unique_ptr<Sprite> bompje;
 
 public:
     GameScene(const std::shared_ptr<GBAEngine> &engine, int level);
 
     std::vector<Sprite *> sprites() override;
+
     std::vector<Background *> backgrounds() override;
 
     void load() override;
+
     void tick(u16 keys) override;
-
 };
-
 
 #endif //GBA_SPRITE_ENGINE_PROJECT_GAME_SCENE_H
--- bomberboy/src/thing.h
+++ bomberboy/src/thing.h
@@ -5,9 +5,15 @@
 #ifndef GBA_SPRITE_ENGINE_PROJECT_THING_H
 #define GBA_SPRITE_ENGINE_PROJECT_THING_H
 
+#include <libgba-sprite-engine/sprites/sprite.h>
 
 class Thing {
+private:
+    std::unique_ptr<Sprite> sprite;
+public:
+    Thing(std::unique_ptr<Sprite> sprite) : sprite(std::move(sprite)) {}
 
+    Sprite* getSprite() { return sprite.get(); }
 };
 
 
