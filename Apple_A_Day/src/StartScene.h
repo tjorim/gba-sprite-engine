ec2e11994fe630ece23db446e8a9a8adfeaf1a42
JonasLommelen <jonas.lommelen@student.uhasselt.be>
2019-01-27 23:35:39 +0100

    startscene.h added (niet mee ge-commit vorige keer?)

---
+++ Apple_A_Day/src/StartScene.h
@@ -0,0 +1,39 @@
+//
+// Created by lloyd on 29-Nov-18.
+//
+
+#ifndef GBA_SPRITE_ENGINE_PROJECT_START_SCENE_H
+#define GBA_SPRITE_ENGINE_PROJECT_START_SCENE_H
+
+#include <libgba-sprite-engine/sprites/sprite.h>
+#include <libgba-sprite-engine/sprites/affine_sprite.h>
+#include <libgba-sprite-engine/scene.h>
+#include <libgba-sprite-engine/background/background.h>
+#include "Direction.h"
+#include "levelControl.h"
+
+class StartScene : public Scene {
+private:
+    std::unique_ptr<Sprite> linkAnimation, sign;
+    std::unique_ptr<Background> bg;
+
+    int scrollX, playerX,playerY;
+    int scrollY;
+    int i;
+    bool isText;
+    bool isNextTileGo;
+    Direction currentDir;
+    levelControl mControl1;
+
+public:
+    std::vector<Sprite *> sprites() override;
+    std::vector<Background *> backgrounds() override;
+
+    StartScene(std::shared_ptr<GBAEngine> engine) : Scene(engine) {}
+
+    void load() override;
+    void tick(u16 keys) override;
+};
+
+
+#endif //GBA_SPRITE_ENGINE_PROJECT_START_SCENE_H
