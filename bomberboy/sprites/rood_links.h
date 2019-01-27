7d0c224c1f137e5a03f30feecf0bb0b0baed4ab8
ruben ballet <ruben.ballet@gmail.com>
2019-01-27 21:32:01 +0100

    sprites

---
+++ bomberboy/sprites/rood_links.h
@@ -0,0 +1,24 @@
+
+//{{BLOCK(rood_links)
+
+//======================================================================
+//
+//	rood_links, 8x16@8, 
+//	+ 2 tiles not compressed
+//	Total size: 128 = 128
+//
+//	Time-stamp: 2019-01-27, 21:21:14
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+const unsigned int rood_linksTiles[32] __attribute__((aligned(4)))=
+{
+	0x08080808,0x083D0808,0x3B3A0808,0x3E3D3A3B,0x3B3B3A08,0x083B083B,0x3B3B3B08,0x3A3B3B3B,
+	0x3B3B3A41,0x3A3B3B3B,0x3B410008,0x3A3B3B3B,0x3B410041,0x083A3B3B,0x3B410008,0x083C3B3B,
+	0x3B3A4108,0x083C3A3B,0x05400808,0x08080B05,0x05400808,0x08084205,0x3D3F0808,0x0808403D,
+	0x053D0008,0x0808000B,0x05000008,0x08080005,0x3D3D0008,0x0808003E,0x00000008,0x08080800,
+};
+
+//}}BLOCK(rood_links)
