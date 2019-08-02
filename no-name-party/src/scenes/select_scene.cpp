543899d050161cebb25dee3fd5eac4289c7423e7
Jorim Tielemans <tielemans.jorim@gmail.com>
2019-08-02 02:31:40 +0200

    This is the select scene now

--- no-name-party/src/scenes/select_scene.cpp
+++ no-name-party/src/scenes/select_scene.cpp
@@ -5,30 +5,30 @@
 #include <libgba-sprite-engine/background/text_stream.h>
 #include <libgba-sprite-engine/gba_engine.h>
 
+#include "select_scene.h"
 #include "game_scene.h"
 //#include "../sound.h"
-#include "start_scene.h"
 
-StartScene::StartScene(const std::shared_ptr<GBAEngine> &engine) : Scene(engine) {}
+SelectScene::SelectScene(const std::shared_ptr<GBAEngine> &engine) : Scene(engine) {}
 
-std::vector<Background *> StartScene::backgrounds() {
+std::vector<Background *> SelectScene::backgrounds() {
     return {};
 }
 
-std::vector<Sprite *> StartScene::sprites() {
+std::vector<Sprite *> SelectScene::sprites() {
     return {};
 }
 
-void StartScene::load() {
+void SelectScene::load() {
     foregroundPalette = std::unique_ptr<ForegroundPaletteManager>(new ForegroundPaletteManager());
     backgroundPalette = std::unique_ptr<BackgroundPaletteManager>(new BackgroundPaletteManager());
 
     //engine->enqueueMusic(cataclysmic_molten_core, sizeof(cataclysmic_molten_core));
 }
 
-void StartScene::tick(u16 keys) {
+void SelectScene::tick(u16 keys) {
     if (keys & KEY_ACCEPT) {
-        engine->setScene(new GameScene(engine, getLevel()));
+        engine->setScene(new GameScene(engine/*, getLevel()*/));
     } else if ((keys & KEY_LEFT) || (keys & KEY_DOWN)) {
         levelDown();
     } else if ((keys & KEY_RIGHT) || (keys & KEY_UP)) {
@@ -40,15 +40,15 @@ void StartScene::tick(u16 keys) {
     TextStream::instance().setText(std::string("Level: ") + std::to_string(level), 10, 1);
 }
 
-int StartScene::getLevel() const {
+int SelectScene::getLevel() const {
     return level;
 }
 
-void StartScene::setLevel(int level) {
-    StartScene::level = level;
+void SelectScene::setLevel(int level) {
+    SelectScene::level = level;
 }
 
-void StartScene::levelUp() {
+void SelectScene::levelUp() {
     if (level < 4) {
         level++;
     } else {
@@ -56,7 +56,7 @@ void StartScene::levelUp() {
     }
 }
 
-void StartScene::levelDown() {
+void SelectScene::levelDown() {
     if (level > 1) {
         level--;
     } else {
--- no-name-party/src/scenes/select_scene.h
+++ no-name-party/src/scenes/select_scene.h
@@ -2,12 +2,13 @@
 // Created by Jorim Tielemans
 //
 
-#ifndef GBA_SPRITE_ENGINE_PROJECT_START_SCENE_H
-#define GBA_SPRITE_ENGINE_PROJECT_START_SCENE_H
+#ifndef GBA_SPRITE_ENGINE_PROJECT_SELECT_SCENE_H
+#define GBA_SPRITE_ENGINE_PROJECT_SELECT_SCENE_H
 
 #include <libgba-sprite-engine/scene.h>
+#include <libgba-sprite-engine/sprites/sprite_builder.h>
 
-class StartScene : public Scene {
+class SelectScene : public Scene {
 private:
     int level = 1;
 
@@ -16,19 +17,16 @@ private:
     void levelDown();
 
 public:
-    StartScene(const std::shared_ptr<GBAEngine> &engine);
+    SelectScene(const std::shared_ptr<GBAEngine> &engine);
 
     std::vector<Sprite *> sprites() override;
-
     std::vector<Background *> backgrounds() override;
 
     void load() override;
-
     void tick(u16 keys) override;
 
     int getLevel() const;
-
     void setLevel(int level);
 };
 
-#endif //GBA_SPRITE_ENGINE_PROJECT_START_SCENE_H
+#endif //GBA_SPRITE_ENGINE_PROJECT_SELECT_SCENE_H
