2a16a9b53c60f326128d63b30868bbdf7dd0ea4b
Jorim Tielemans <tielemans.jorim@gmail.com>
2019-08-02 02:33:12 +0200

    These old sprites aren't used anymore

--- no-name-party/src/scenes/start_scene.cpp
+++ no-name-party/src/scenes/start_scene.cpp
@@ -9,10 +9,6 @@
 #include "game_scene.h"
 //#include "../sound.h"
 #include "../../sprites/background_rainbow.h"
-#include "../../sprites/balloon_blue.h"
-#include "../../sprites/balloon_green.h"
-#include "../../sprites/balloon_pink.h"
-#include "../../sprites/balloon_yellow.h"
 #include "../../sprites/press_start_left.h"
 #include "../../sprites/press_start_right.h"
 #include "../../sprites/shared.h"
@@ -27,11 +23,6 @@ std::vector<Background *> StartScene::backgrounds() {
 
 std::vector<Sprite *> StartScene::sprites() {
     std::vector < Sprite * > sprites;
-
-    sprites.push_back(balloon_blue.get());
-    sprites.push_back(balloon_green.get());
-    sprites.push_back(balloon_pink.get());
-    sprites.push_back(balloon_yellow.get());
 
     for (auto& balloon : balloons) {
         sprites.push_back(balloon->getSprite());
@@ -68,78 +59,28 @@ void StartScene::load() {
             .withLocation(GBA_SCREEN_WIDTH / 2, GBA_SCREEN_HEIGHT / 2 - 32)
             .buildPtr();
 
-    balloon_blue = spriteBuilder->withData(balloon_blueTiles, sizeof(balloon_blueTiles))
-            .withSize(SIZE_16_32)
-            .withLocation(GBA_SCREEN_WIDTH + 20, GBA_SCREEN_HEIGHT + 20)
-            .buildPtr();
+    balloons.push_back(
+        std::unique_ptr<Balloon>(new Balloon(Colour::BLUE, 10, -1, 0))
+    );
 
-    balloon_green = spriteBuilder->withData(balloon_greenTiles, sizeof(balloon_greenTiles))
-            .withSize(SIZE_16_32)
-            .withLocation(GBA_SCREEN_WIDTH + 20, GBA_SCREEN_HEIGHT + 20)
-            .buildPtr();
+    balloons.push_back(
+        std::unique_ptr<Balloon>(new Balloon(Colour::GREEN, 50, -2, 0))
+    );
 
-    balloon_pink = spriteBuilder->withData(balloon_pinkTiles, sizeof(balloon_pinkTiles))
-            .withSize(SIZE_16_32)
-            .withLocation(GBA_SCREEN_WIDTH + 20, GBA_SCREEN_HEIGHT + 20)
-            .buildPtr();
+    balloons.push_back(
+        std::unique_ptr<Balloon>(new Balloon(Colour::PINK, 150, -1, 40))
+    );
 
-    balloon_yellow = spriteBuilder->withData(balloon_yellowTiles, sizeof(balloon_yellowTiles))
-            .withSize(SIZE_16_32)
-            .withLocation(GBA_SCREEN_WIDTH + 20, GBA_SCREEN_HEIGHT + 20)
-            .buildPtr();
+    balloons.push_back(
+        std::unique_ptr<Balloon>(new Balloon(Colour::YELLOW, 200, -2, 40))
+    );
 
-        balloons.push_back(
-            std::unique_ptr<Balloon>(new Balloon(spriteBuilder
-            ->withLocation(10, 20)
-            .buildWithDataOf(*balloon_blue.get())))
-        );
-        balloons.push_back(
-            std::unique_ptr<Balloon>(new Balloon(spriteBuilder
-            ->withLocation(20, 40)
-            .buildWithDataOf(*balloon_green.get())))
-        );
-/*
-    balloon_blue = std::unique_ptr<Sprite>(new Sprite());
-
-    std::unique_ptr<Sprite> sprite;
-
-    Player::Player(int xCo, int yCo) : xCo(xCo), yCo(yCo) {
-    setXCo(xCo);
-    setYCo(yCo);
-
-
-
-Balloon::Balloon(int xCo, int yCo) : xCo(xCo), yCo(yCo) {
-    setXCo(xCo);
-    setYCo(yCo);
-    SpriteBuilder<Sprite> spriteBuilder;
-    setSprite(spriteBuilder
-            .withData(balloon_blueTiles, sizeof(balloon_blueTiles))
-            .withSize(SIZE_16_32)
-            //.withLocation(GBA_SCREEN_WIDTH / 2 - 32, GBA_SCREEN_HEIGHT / 2 - 32)
-            .withLocation(xCo, yCo)
-            .buildPtr());
-}
-    
-    setSprite(spriteBuilder
-            .withData(Luigi_leftTiles, sizeof(Luigi_leftTiles))
-            .withSize(SIZE_32_32)
-            .withAnimated(2, 4)
-            //.withLocation(GBA_SCREEN_WIDTH / 2 - 32, GBA_SCREEN_HEIGHT / 2 - 32)
-            .withLocation(xCo, yCo)
-            .buildPtr());
-}
- */
     //engine->enqueueMusic(cataclysmic_molten_core, sizeof(cataclysmic_molten_core));
 }
 
 void StartScene::tick(u16 keys) {
-    if (keys & KEY_ACCEPT) {
-        engine->setScene(new GameScene(engine));
-    } else if ((keys & KEY_LEFT) || (keys & KEY_DOWN)) {
-        //levelDown();
-    } else if ((keys & KEY_RIGHT) || (keys & KEY_UP)) {
-        //levelUp();
+    if (keys & KEY_START) {
+        engine->setScene(new SelectScene(engine));
     }
 
     TextStream::instance().setText(std::string("Intro scene"), 10, 1);
