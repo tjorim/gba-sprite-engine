b51ca13608441bbd3f0f4529799816edb089e890
Jorim Tielemans <tielemans.jorim@gmail.com>
2019-08-05 00:01:35 +0200

    Revert "Split up sprite further"
    This reverts commit 9f38f86b58cfc526895fa34994d6bd05fb56c06d.

--- no-name-party/sprites/dice_blue.h
+++ no-name-party/sprites/dice_blue.h
@@ -7,7 +7,7 @@
 //	+ 40 tiles not compressed
 //	Total size: 2560 = 2560
 //
-//	Time-stamp: 2019-08-04, 23:59:37
+//	Time-stamp: 2019-08-04, 23:20:39
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
 //	( http://www.coranac.com/projects/#grit )
 //
