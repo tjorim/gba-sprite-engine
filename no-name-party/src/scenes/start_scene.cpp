10315efbc62f213e0a25afa55cf734ad84cc82c8
Jorim Tielemans <tielemans.jorim@gmail.com>
2019-08-05 00:42:31 +0200

    Use press start sprites without animation

--- no-name-party/src/scenes/start_scene.cpp
+++ no-name-party/src/scenes/start_scene.cpp
@@ -9,8 +9,8 @@
 #include "select_scene.h"
 //#include "../sound.h"
 #include "../../sprites/background_rainbow.h"
-#include "../../sprites/press_start_left.h"
-#include "../../sprites/press_start_right.h"
+#include "../../sprites/press_start_left_clone.h"
+#include "../../sprites/press_start_right_clone.h"
 #include "../../sprites/shared.h"
 
 StartScene::StartScene(const std::shared_ptr<GBAEngine> &engine) : Scene(engine) {}
@@ -28,8 +28,8 @@ std::vector<Sprite *> StartScene::sprites() {
         sprites.push_back(balloon->getSprite());
     }
 
-    //sprites.push_back(press_start_left.get());
-    //sprites.push_back(press_start_right.get());
+    sprites.push_back(press_start_left.get());
+    sprites.push_back(press_start_right.get());
 
     TextStream::instance().setText(std::string("Sprites ") + std::to_string(sprites.size()), 12, 1);
     return sprites;
@@ -47,30 +47,25 @@ void StartScene::load() {
 
     spriteBuilder = std::unique_ptr<SpriteBuilder<Sprite>>(new SpriteBuilder<Sprite>);
 
-    press_start_left = spriteBuilder->withData(press_start_leftTiles, sizeof(press_start_leftTiles))
+    press_start_left = spriteBuilder->withData(press_start_left_cloneTiles, sizeof(press_start_left_cloneTiles))
             .withSize(SIZE_64_32)
-            .withAnimated(22, 2)
             .withLocation(GBA_SCREEN_WIDTH / 2 - 64, GBA_SCREEN_HEIGHT / 2 - 32)
             .buildPtr();
 
-    press_start_right = spriteBuilder->withData(press_start_rightTiles, sizeof(press_start_rightTiles))
+    press_start_right = spriteBuilder->withData(press_start_right_cloneTiles, sizeof(press_start_right_cloneTiles))
             .withSize(SIZE_64_32)
-            .withAnimated(22, 2)
             .withLocation(GBA_SCREEN_WIDTH / 2, GBA_SCREEN_HEIGHT / 2 - 32)
             .buildPtr();
 
     balloons.push_back(
         std::unique_ptr<Balloon>(new Balloon(Colour::BLUE, 10, -1, 0))
     );
-
     balloons.push_back(
         std::unique_ptr<Balloon>(new Balloon(Colour::GREEN, 50, -2, 0))
     );
-
     balloons.push_back(
         std::unique_ptr<Balloon>(new Balloon(Colour::PINK, 150, -1, 40))
     );
-
     balloons.push_back(
         std::unique_ptr<Balloon>(new Balloon(Colour::YELLOW, 200, -2, 40))
     );
