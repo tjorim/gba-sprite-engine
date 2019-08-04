72c6b6d0727445b238500d61eeadd237eb664171
Jorim Tielemans <tielemans.jorim@gmail.com>
2019-08-05 00:19:34 +0200

    Revert "All frames of all parts in a single file"
    This reverts commit 1e52873f866844f90f3e42af4e8c92eb5066b946.

--- no-name-party/sprites/cloud_right.h
+++ no-name-party/sprites/cloud_right.h
@@ -7,7 +7,7 @@
 //	+ 64 tiles not compressed
 //	Total size: 4096 = 4096
 //
-//	Time-stamp: 2019-08-05, 00:17:58
+//	Time-stamp: 2019-08-04, 23:59:37
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
 //	( http://www.coranac.com/projects/#grit )
 //
