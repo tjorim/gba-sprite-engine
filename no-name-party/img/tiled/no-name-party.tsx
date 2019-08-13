ac7f85083c924778f11d15f557f727d06a6821c4
Jorim Tielemans <tielemans.jorim@gmail.com>
2019-08-13 19:58:58 +0200

    Include Tiled tiles
    and tileset

---
+++ no-name-party/img/tiled/no-name-party.tsx
@@ -0,0 +1,148 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="no-name-party" tilewidth="16" tileheight="16" tilecount="48" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="tiles/arrow_up.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="tiles/arrow_right.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="tiles/space_mini_game.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="16" source="tiles/space_empty.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="16" source="tiles/space_yellow.png"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="16" source="tiles/space_green_dice.png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="16" source="tiles/space_red_minus.png"/>
+ </tile>
+ <tile id="7">
+  <image width="16" height="16" source="tiles/road_bottom_right_inside.png"/>
+ </tile>
+ <tile id="8">
+  <image width="16" height="16" source="tiles/road_bottom_side.png"/>
+ </tile>
+ <tile id="9">
+  <image width="16" height="16" source="tiles/road_right_side.png"/>
+ </tile>
+ <tile id="10">
+  <image width="16" height="16" source="tiles/road_top_left_corner.png"/>
+ </tile>
+ <tile id="11">
+  <image width="16" height="16" source="tiles/road_top_left_inside.png"/>
+ </tile>
+ <tile id="12">
+  <image width="16" height="16" source="tiles/road_top_right_corner.png"/>
+ </tile>
+ <tile id="13">
+  <image width="16" height="16" source="tiles/road_top_right_inside.png"/>
+ </tile>
+ <tile id="14">
+  <image width="16" height="16" source="tiles/road_top_side.png"/>
+ </tile>
+ <tile id="15">
+  <image width="16" height="16" source="tiles/tree_bottom_left.png"/>
+ </tile>
+ <tile id="16">
+  <image width="16" height="16" source="tiles/tree_bottom_right.png"/>
+ </tile>
+ <tile id="17">
+  <image width="16" height="16" source="tiles/tree_top_left.png"/>
+ </tile>
+ <tile id="18">
+  <image width="16" height="16" source="tiles/tree_top_right.png"/>
+ </tile>
+ <tile id="19">
+  <image width="16" height="16" source="tiles/arrow_down.png"/>
+ </tile>
+ <tile id="20">
+  <image width="16" height="16" source="tiles/arrow_left.png"/>
+ </tile>
+ <tile id="21">
+  <image width="16" height="16" source="tiles/grass.png"/>
+ </tile>
+ <tile id="22">
+  <image width="16" height="16" source="tiles/island_bottom_left_corner.png"/>
+ </tile>
+ <tile id="23">
+  <image width="16" height="16" source="tiles/island_bottom_right_corner.png"/>
+ </tile>
+ <tile id="24">
+  <image width="16" height="16" source="tiles/island_bottom_side.png"/>
+ </tile>
+ <tile id="25">
+  <image width="16" height="16" source="tiles/island_left_side.png"/>
+ </tile>
+ <tile id="26">
+  <image width="16" height="16" source="tiles/island_right_side.png"/>
+ </tile>
+ <tile id="27">
+  <image width="16" height="16" source="tiles/island_top_right_inside.png"/>
+ </tile>
+ <tile id="28">
+  <image width="16" height="16" source="tiles/road_bottom_left_corner.png"/>
+ </tile>
+ <tile id="29">
+  <image width="16" height="16" source="tiles/road_bottom_left_inside.png"/>
+ </tile>
+ <tile id="30">
+  <image width="16" height="16" source="tiles/road_bottom_right_corner.png"/>
+ </tile>
+ <tile id="31">
+  <image width="16" height="16" source="tiles/water.png"/>
+ </tile>
+ <tile id="32">
+  <image width="16" height="16" source="tiles/road_left_side.png"/>
+ </tile>
+ <tile id="33">
+  <image width="16" height="16" source="tiles/start_left_6.png"/>
+ </tile>
+ <tile id="34">
+  <image width="16" height="16" source="tiles/start_left_7.png"/>
+ </tile>
+ <tile id="35">
+  <image width="16" height="16" source="tiles/start_left_8.png"/>
+ </tile>
+ <tile id="36">
+  <image width="16" height="16" source="tiles/start_left_9.png"/>
+ </tile>
+ <tile id="37">
+  <image width="16" height="16" source="tiles/start_left_10.png"/>
+ </tile>
+ <tile id="38">
+  <image width="16" height="16" source="tiles/start_left_11.png"/>
+ </tile>
+ <tile id="39">
+  <image width="16" height="16" source="tiles/start_left_12.png"/>
+ </tile>
+ <tile id="40">
+  <image width="16" height="16" source="tiles/start_left_1.png"/>
+ </tile>
+ <tile id="41">
+  <image width="16" height="16" source="tiles/start_left_2.png"/>
+ </tile>
+ <tile id="42">
+  <image width="16" height="16" source="tiles/start_left_3.png"/>
+ </tile>
+ <tile id="43">
+  <image width="16" height="16" source="tiles/start_left_4.png"/>
+ </tile>
+ <tile id="44">
+  <image width="16" height="16" source="tiles/start_left_5.png"/>
+ </tile>
+ <tile id="45">
+  <image width="16" height="16" source="tiles/island_top_side.png"/>
+ </tile>
+ <tile id="46">
+  <image width="16" height="16" source="tiles/island_top_right_corner.png"/>
+ </tile>
+ <tile id="47">
+  <image width="16" height="16" source="tiles/island_top_left_corner.png"/>
+ </tile>
+</tileset>
