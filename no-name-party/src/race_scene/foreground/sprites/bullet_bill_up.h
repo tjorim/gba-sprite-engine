97bb6c3f19df57cdd5cdfdf07b3f12c4d8d8599f
Jorim Tielemans <tielemans.jorim@gmail.com>
2019-08-17 17:27:14 +0200

    Add mushroom sprite
    indicates a life

--- no-name-party/src/race_scene/foreground/sprites/bullet_bill_up.h
+++ no-name-party/src/race_scene/foreground/sprites/bullet_bill_up.h
@@ -7,7 +7,7 @@
 //	+ 16 tiles not compressed
 //	Total size: 1024 = 1024
 //
-//	Time-stamp: 2019-08-15, 18:15:17
+//	Time-stamp: 2019-08-17, 17:12:23
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
 //	( http://www.coranac.com/projects/#grit )
 //
--- no-name-party/src/race_scene/foreground/sprites/shared_race_scene.h
+++ no-name-party/src/race_scene/foreground/sprites/shared_race_scene.h
@@ -4,19 +4,20 @@
 //======================================================================
 //
 //	shared_race_scene, 16x16@8, 
-//	+ palette 24 entries, not compressed
-//	Total size: 48 = 48
+//	+ palette 32 entries, not compressed
+//	Total size: 64 = 64
 //
-//	Time-stamp: 2019-08-15, 18:15:17
+//	Time-stamp: 2019-08-17, 17:12:23
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
 //	( http://www.coranac.com/projects/#grit )
 //
 //======================================================================
 
-const unsigned short shared_race_scenePal[24] __attribute__((aligned(4)))=
+const unsigned short shared_race_scenePal[32] __attribute__((aligned(4)))=
 {
 	0x0000,0x4210,0x18C6,0x6318,0x2108,0x294A,0x77BD,0x318C,
-	0x4E73,0x14EA,0x0421,0x39CE,0x59EF,0x14EA,0x6AB5,0x2041,
+	0x4E73,0x14EA,0x0421,0x39CE,0x56FB,0x14B6,0x1068,0x73DF,
+	0x0C7F,0x677D,0x7FFF,0x5ADF,0x59EF,0x14EA,0x6AB5,0x2041,
 	0x29F5,0x77BD,0x3908,0x225A,0x2863,0x6F18,0x0400,0x7FFF,
 };
 
